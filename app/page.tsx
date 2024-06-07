f387eb905391af7b13850652d41054307a593644
thapasushant52 <thapasushant52@gmail.com>
2024-06-07 10:40:09 +0000

    data from excel grabbing logic done

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-'use client';
-import Image from "next/image";
-import { useEffect, useState } from "react";
 import * as XLSX from 'xlsx';
 type FileDataProps ={
   [index: number]: number | string;
@@ -10,74 +7,103 @@ type FileDataProps ={
 }
 export default function Home() {
  
-  const [excelFile, setExcelFile] = useState<String | ArrayBuffer | null | undefined>(null);
-  const [excelFileError, setExcelFileError] = useState<null| String>(null);
-  const [excelFileData, setExcelFileData] = useState<null| FileDataProps[]>(null);
-  const [nameData,setNameData] = useState<String[] | null>(null);
+  // const [excelFile, setExcelFile] = useState<String | ArrayBuffer | null | undefined>(null);
+  // const [excelFileError, setExcelFileError] = useState<null| String>(null);
+  // const [excelFileData, setExcelFileData] = useState<null| FileDataProps[]>(null);
+  // const [nameData,setNameData] = useState<String[] | null>(null);
  
  
  
-  const FileType = "application/vnd.ms-excel";
+  // const FileType = "application/vnd.ms-excel";
  
-  const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
-    if (!e.target.files) return;
-    let selectedFiles = e.target.files[0];
-    if(selectedFiles){
-      if(selectedFiles&&FileType.includes(selectedFiles.type)){
-        let reader = new FileReader();
-        reader.readAsArrayBuffer(selectedFiles);
-        reader.onload= (e) => {
-          setExcelFileError(null);
-          setExcelFile(e.target?.result);
-        }
+  // const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+  //   if (!e.target.files) return;
+  //   let selectedFiles = e.target.files[0];
+  //   if(selectedFiles){
+  //     if(selectedFiles&&FileType.includes(selectedFiles.type)){
+  //       let reader = new FileReader();
+  //       reader.readAsArrayBuffer(selectedFiles);
+  //       reader.onload= (e) => {
+  //         setExcelFileError(null);
+  //         setExcelFile(e.target?.result);
+  //       }
  
  
-      }
-    } else{
-      setExcelFileError("Please select excel file");
-      setExcelFile(null);
-    }
+  //     }
+  //   } else{
+  //     setExcelFileError("Please select excel file");
+  //     setExcelFile(null);
+  //   }
  
+  // }
+  // const handleSubmit = (e:React.FormEvent<HTMLFormElement>)=>{
+  //   e.preventDefault();
+  //   if(excelFile!==null){
+  //     const workbook = XLSX.read(excelFile,{type:"buffer"});
+  //     const worksheetName = workbook.SheetNames[0];
+  //     const worksheet = workbook.Sheets[worksheetName];
+  //     const data = XLSX.utils.sheet_to_json(worksheet);
+
+      
+  //     setExcelFileData(data as FileDataProps[]);
+      
+      
+ 
+  //   }else {
+  //     setExcelFileData(null);
+  //   }
+ 
+  // }
+  // console.log(excelFileData);
+  // console.log(excelFileError);
+  // let temp:String[] = [];   //Get Any pre-stored items
+  // useEffect(()=>{
+  //   if(excelFileData){
+
+  //     excelFileData.map((row,index)=>{
+
+  //   temp.push(row["First Name"]);
+  //         console.log(row["First Name"]);
+  //   })
+  //   }
+  //   setNameData(temp);
+  // },[excelFileData])
+  // console.log(nameData);
+  type ExcelDataProps = {
+    "Phone": number
   }
-  const handleSubmit = (e:React.FormEvent<HTMLFormElement>)=>{
-    e.preventDefault();
-    if(excelFile!==null){
-      const workbook = XLSX.read(excelFile,{type:"buffer"});
+
+  async function upload(data:FormData){
+    'use server'
+
+    const file: null | File = data.get("file") as unknown as File;
+    const fileType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    if(!file) throw("No file uploaded");
+    if(!fileType.includes(file.type)) throw("Please upload a spreadsheet file");
+
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
+          const workbook = XLSX.read(buffer,{type:"buffer"});
       const worksheetName = workbook.SheetNames[0];
       const worksheet = workbook.Sheets[worksheetName];
-      const data = XLSX.utils.sheet_to_json(worksheet);
+      const excelData:ExcelDataProps[] = XLSX.utils.sheet_to_json(worksheet);
+      console.log(excelData);
 
-      
-      setExcelFileData(data as FileDataProps[]);
-      
-      
- 
-    }else {
-      setExcelFileData(null);
-    }
- 
+      let ActualData:number[] = [];
+
+      excelData.map((e)=>{
+        console.log(e.Phone);
+        ActualData.push(e.Phone);
+      })
+    
   }
-  console.log(excelFileData);
-  console.log(excelFileError);
-  let temp:String[] = [];   //Get Any pre-stored items
-  useEffect(()=>{
-    if(excelFileData){
 
-      excelFileData.map((row,index)=>{
-
-    temp.push(row["First Name"]);
-          console.log(row["First Name"]);
-    })
-    }
-    setNameData(temp);
-  },[excelFileData])
-  console.log(nameData);
 
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <form autoComplete="off" onSubmit={handleSubmit}>
-      <input type="file" onChange={handleChange} required/>
+     <form action={upload} autoComplete="off">
+      <input type="file" name="file" required/>
       <button type="submit">Submit</button>
  
      </form>
